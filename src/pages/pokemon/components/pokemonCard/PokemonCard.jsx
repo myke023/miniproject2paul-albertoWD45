686617a70810c480b6867c8801d8be342a1928b1
Linggit20 <botonesallan7@gmai.com>
2023-04-10 11:24:15 +0800

    add hover effect

--- src/pages/pokemon/components/pokemonCard/PokemonCard.jsx
+++ src/pages/pokemon/components/pokemonCard/PokemonCard.jsx
@@ -1,12 +1,12 @@
 import Card from "react-bootstrap/Card"
 import { typeColors } from "./TypeOfColor"
 
-const PokemonCard = ({ pokemon }) => {
+const PokemonCard = ({ pokemon, className }) => {
   const primaryType = pokemon.types[0].type.name
   const backgroundColor = typeColors[primaryType]
 
   return (
-      <Card className="py-3" style={{ backgroundColor }}>
+      <Card className={`py-3 ${className}`} style={{ backgroundColor }}>
         <Card.Img variant="top" src={pokemon.sprites.other["official-artwork"].front_default} className="pokemon-img" />
         <Card.Body>
           <Card.Title className="pokemon-name text-center" style={{ textTransform: "capitalize"}}>
