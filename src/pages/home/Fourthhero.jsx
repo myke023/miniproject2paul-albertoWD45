e56e07eb7dfbea9d16ed9aa837e9a4da26fae4d2
Linggit20 <botonesallan7@gmai.com>
2023-04-11 11:04:19 +0800

    fix typo | change class to className

--- src/pages/home/Fourthhero.jsx
+++ src/pages/home/Fourthhero.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 const Fourthhero = () => {
   return (
-<div class="container text-center"> 
-  <div class="row"> <div className='span31 margintop-10 marginright20'><span>Latest Updates</span></div>
-    <div class="col"> 
-
-    </div>
+<div className="container text-center"> 
+  <div className="row"> <div className='span31 margintop-10 marginright20'><span>Latest Updates</span></div>
   </div>
 </div>
   )
